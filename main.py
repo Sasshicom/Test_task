095aa889f5304b4e1bb2561d6902ff542b77070c
Alexander <workmail1151A@mail.ru>
2021-02-20 03:21:56 +0700

    Delete 1 string))

--- main.py
+++ main.py
@@ -1,47 +1,56 @@
 import numpy as np
 
-if __name__ == '__main__':
-    def main_function(n):
-        #Блок кода с генерацией массивов случайной размерности
-        #*****************************************************
 
-        main_list = []
-        for j in range(n):
-            internal_list = []
-            k = np.random.randint(1, 10)
-            for i in range(k):
-                internal_list.append(np.random.randint(1, 100))
-            main_list.append(internal_list)
+def main_function(n):
+    # Блок кода с генерацией массивов случайной размерности
+    # *****************************************************
 
-        # *****************************************************
-        #Раскоментив эту строку можно посмотреть сгенерированные массивы
-        #print("First generate:", main_list)
+    main_list = []
+    for j in range(n):
+        internal_list = []
+        for i in range(1, n+1):
+            k = [np.random.randint(1, n)]
+            print(k)
+        for i in range(k):
+            internal_list.append(np.random.randint(1, 100))
 
-        #Блок кода с просмотром длины массивов, по условию задачи не должно быть одинаковых длин
-        # *****************************************************
+        main_list.append(internal_list)
+    # k = [70,15,1,68,150]
+    # for i in k:
+    #     print(i)
+    #     random 1, i
+        # for j in range(i):
+        #     print(i)
+    # *****************************************************
+    # Раскоментив эту строку можно посмотреть сгенерированные массивы
+    # print("First generate:", main_list)
 
-        d3 = [len(i) for i in main_list]
-        print('Список длинн:', d3)
-        for i in range(n):
-            for j in range(i, n-1):
-                if d3[i] == d3[j+1]:
-                    print('Есть одинаковые длины, введите число снова')
-                    quit()
+    # Блок кода с просмотром длины массивов, по условию задачи не должно быть одинаковых длин
+    # *****************************************************
 
-        # *****************************************************
+    d3 = [len(i) for i in main_list]
+    print('Список длинн:', d3)
+    for i in range(n):
+        for j in range(i, n - 1):
+            if d3[i] == d3[j + 1]:
+                print('Есть одинаковые длины, введите число снова')
+                quit()
 
-        #Блок кода с сортировкой массивов в соответствии заданию
-        # *****************************************************
-        for i in range(n):
-            if i % 2 == 0:
-                main_list[i].sort(key=None, reverse=False)
+    # *****************************************************
 
-            elif i % 2 != 0:
-                main_list[i].sort(key=None, reverse=True)
+    # Блок кода с сортировкой массивов в соответствии заданию
+    # *****************************************************
+    for i in range(n):
+        if i % 2 == 0:
+            main_list[i].sort(key=None, reverse=False)
 
-        # *****************************************************
+        elif i % 2 != 0:
+            main_list[i].sort(key=None, reverse=True)
 
-        return main_list
+    # *****************************************************
+
+    return main_list
+
 
 sorted_massive = main_function(int(input("Введите число: ")))
 print(sorted_massive)
